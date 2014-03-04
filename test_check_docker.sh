2b5a1b7b8a8eefa30cd3ee19b6d7204e58ab3d46
Ho Ming Li <homingli@gmail.com>
2014-03-05 07:39:48 +0800

    rename without .sh; simplified test script; added comments to script

--- test_check_docker.sh
+++ test_check_docker.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-CHECK_DOCKER="./check_docker.sh"
+# mini test suite for check_docker
+# only runs cases, but does not check result
 
-echo 
-echo ==Test 1== OUTPUT USAGE
-echo 
+G_TNUM=1
+
+test_header() {
+    # test_header <title>
+    echo 
+    echo == TEST $G_TNUM == $1 ==
+    echo 
+    G_TNUM=$((G_TNUM+1))
+}
+
+CHECK_DOCKER="./check_docker"
+
+test_header "OUTPUT USAGE"
 $CHECK_DOCKER -?
 
-echo 
-echo ==Test 2== OUTPUT HELP
-echo 
+test_header "OUTPUT HELP"
 $CHECK_DOCKER --help
 
-echo 
-echo ==Test 3== NO_ARG
-echo 
+test_header "NO ARGUMENT"
 $CHECK_DOCKER
 
-echo 
-echo ==Test 4== HOST: localhost, PORT: 4243
-echo 
+test_header "HOST: localhost, PORT: 4243"
 $CHECK_DOCKER --host localhost --port 4243
 
-echo
-echo ==Test 5== SOCKET FILE: /var/run/docker.sock
-echo 
+test_header "SOCKET FILE: /var/run/docker.sock"
 $CHECK_DOCKER -f /var/run/docker.sock
